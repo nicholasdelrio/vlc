8a5740f4a584da2e15824f17a77e573f887e7f7e
Nick <nicholas.delrio@gmail.com>
2013-07-21 13:54:47 -0600

    fixed bug in csv

--- harvester/src/main/java/edu/utep/cybershare/vlc/geocoding/InstitutionCSV.java
+++ harvester/src/main/java/edu/utep/cybershare/vlc/geocoding/InstitutionCSV.java
@@ -1,9 +1,14 @@
 package edu.utep.cybershare.vlc.geocoding;
 
+import java.awt.Point;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.StringWriter;
+import java.util.Hashtable;
 import java.util.List;
+
+import au.com.bytecode.opencsv.CSVReader;
 
 import edu.utep.cybershare.vlc.ontology.Institution;
 import edu.utep.cybershare.vlc.sources.ProjectSource;
@@ -11,15 +16,43 @@ import edu.utep.cybershare.vlc.sources.nsf.NSFAwards;
 
 public class InstitutionCSV {
 
-	public List<Institution> institutions;
+	private List<Institution> institutions;
+	private Hashtable<String,Point> institutionToCoordinates;
 	
 	public InstitutionCSV(List<Institution> institutions){
 		this.institutions = institutions;
 	}
 	
+	public InstitutionCSV(File csvFile){
+	    institutionToCoordinates = new Hashtable <String, Point>();
+		
+		try{
+			CSVReader reader = new CSVReader(new FileReader(csvFile));
+			List<String[]> geocodedRecords = reader.readAll();
+		    String lat;
+		    String lon;
+		    String name;
+		    Point location;
+			for(String[] entry : geocodedRecords){
+		    	name = entry[4];
+				lat = entry[29];
+		    	lon = entry[30];
+		    	location = new Point();
+		    	location.setLocation(Double.valueOf(lon), Double.valueOf(lat));
+		    	institutionToCoordinates.put(name, location);
+		    }
+			reader.close();
+		}catch(Exception e){e.printStackTrace();}
+	}
+	
+	public Point getCoordinates(String institutionName){
+		String cleanedName = institutionName.replaceAll(",", "");
+		return this.institutionToCoordinates.get(cleanedName);
+	}
+	
 	public void dumpCSVFile(){
 		StringWriter writer = new StringWriter();
-		writer.append("Address, City, State, Zip, Institution Name\n");
+		writer.append("Address, City, State, Zip, Name\n");
 		
 		String address;
 		String city;
@@ -28,11 +61,11 @@ public class InstitutionCSV {
 		String institutionName;
 		for(Institution anInstitution : institutions){
 			
-			address = anInstitution.getHasAddress().replaceAll(",", "");
-			city = anInstitution.getHasCity().replaceAll(", ", "");
-			state = anInstitution.getHasState().replaceAll(",", "");
-			zipCode = anInstitution.getHasZipCode().replaceAll(",", "");
-			institutionName = anInstitution.getHasName().replaceAll(",", "");
+			address = anInstitution.getHasAddress().replaceAll(",", " ");
+			city = anInstitution.getHasCity().replaceAll(",", " ");
+			state = anInstitution.getHasState().replaceAll(",",  "");
+			zipCode = anInstitution.getHasZipCode().replaceAll(",", " ");
+			institutionName = anInstitution.getHasName().replaceAll(",", " ");
 			
 			writer.append(address + ", ");
 			writer.append(city + ", ");
